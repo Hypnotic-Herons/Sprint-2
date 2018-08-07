cd8b6779035b586c8bf7052f9825b045c4dbcaaa
Meghan Debity <mdebity@gmail.com>
2018-08-07 10:37:15 -0500

    added notes for model

--- HRIT/models/department.py
+++ HRIT/models/department.py
@@ -2,6 +2,12 @@ from django.db import models
 
 
 class Department(models.Model):
+	"""
+	Authors: Meghan Debity and Jessica Swift
+	Purpose: Create department model
+	"""
+
+
 	name = models.CharField(max_length = 30)
 	budget = models.CharField(max_legth = 30)
 	supervisorId = models.ForeignKey('Employee', on_delete=models.CASCADE)
