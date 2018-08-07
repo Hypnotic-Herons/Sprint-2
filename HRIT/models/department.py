523cecd78b544b818956930856eea77327945070
Meghan Debity <mdebity@gmail.com>
2018-08-07 10:35:20 -0500

    md and js added department model with meta class

--- HRIT/models/department.py
+++ HRIT/models/department.py
@@ -1,1 +1,10 @@
-#model for department
+from django.db import models
+
+
+class Department(models.Model):
+	name = models.CharField(max_length = 30)
+	budget = models.CharField(max_legth = 30)
+	supervisorId = models.ForeignKey('Employee', on_delete=models.CASCADE)
+
+	class Meta:
+		db_table = "department"
