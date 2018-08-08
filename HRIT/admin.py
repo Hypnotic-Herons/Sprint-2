1ae6d4c330f7c45b416bf451f623c6f34c659d8f
teaglebuilt <dillan.teagle.va@gmail.com>
2018-08-08 10:24:10 -0500

    changes/2pac

--- HRIT/admin.py
+++ HRIT/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from HRIT.models import Department, Computer, Employee
 
 
+admin.site.register(Employee)
+admin.site.register(Department)
+admin.site.register(Computer)
 
