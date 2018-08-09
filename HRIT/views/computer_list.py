e11a3b37c326692c58c0eab9659cf36b3be00a70
Deanna Vickers <deanna2000@gmail.com>
2018-08-09 11:23:20 -0500

    changed computerlist html filename

--- HRIT/views/computer_list.py
+++ HRIT/views/computer_list.py
@@ -6,7 +6,7 @@ class Computer_List(TemplateView):
 	Authors: Dillan Teagle
 	Purpose: Create Computer List View
 	"""
-	template_name = 'HRIT/computers.html'
+	template_name = 'HRIT/computers_list.html'
 
 	def computer_list(self):
 		computers = Computer.objects.all()
