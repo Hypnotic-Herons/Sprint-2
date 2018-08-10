0a2acd36dfcb11a2b5789d3e279d51843c9969a4
Meghan Debity <mdebity@gmail.com>
2018-08-10 10:58:50 -0500

    first department test run and passed, matt kelly, meghan debity

--- HRIT/views/department.py
+++ HRIT/views/department.py
@@ -1,11 +1,9 @@
 from HRIT.models import Department
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
-class Department_List_View(TemplateView):
+class Department_List_View(ListView):
     """[summary]
     API endpoint that allows departments to be viewed and edited.
     """
-    template_name = 'HRIT/departments.html'
-    def department_list(self):
-        departments = Department.objects.all()
-        return departments
+    model = Department
+    context_object_name = 'department_list'
