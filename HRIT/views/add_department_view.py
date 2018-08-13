c24bde20468ddd16ef6f6d097cd25bdfdb99f212
Jessica Swift <jessica.nicole.swift@gmail.com>
2018-08-13 16:06:30 -0500

    fixed issues, thanks Deanna

--- HRIT/views/add_department_view.py
+++ HRIT/views/add_department_view.py
@@ -7,18 +7,18 @@ from HRIT.forms import DepartmentForm
 
 
 class DepartmentFormView(FormView):
-    template_name = 'HRIT/department_list.html'
+    template_name = 'HRIT/department_form.html'
     form_class = DepartmentForm
 
-    success_url = '/HRIT/Department/'
+    success_url = '/HRIT/departments/'
 
     def form_valid(self, form):
     # This method is called when valid form data has been POSTed.
     # It should return an HttpResponse.
         form.save()
-        return redirect('/HRIT/department/')
+        return redirect('/HRIT/departments/')
 
 
-class Department_Delete(DeleteView):
-    model = Department
-    success_url = reverse_lazy('/HRIT/department/')
+# class Department_Delete(DeleteView):
+#     model = Department
+#     success_url = reverse_lazy('/HRIT/departments/')
